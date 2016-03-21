e39465e551d01ec1171800e38bed074a09b13bdc
Isart Canyameres <isart.canyameres@i2cat.net>
2016-03-21 15:11:48 +0100

    Fix error with types

--- src/test/python/vpn/serverchecker.py
+++ src/test/python/vpn/serverchecker.py
@@ -16,30 +16,32 @@ def main():
     # cheat configuration
     print "Configuring server..."
     r = requests.post(url_dh, data={}, files={'file': ('dh2048.pem', 'SAMPLE_DH\n')})
-    print "Response" + r
+    print r
     r.raise_for_status()
     r = requests.post(url_server_conf, data={}, files={'file': ('server.conf', 'SAMPLE_CONF\n')})
-    print "Response" + r
+    print r
     r.raise_for_status()
     r = requests.post(url_ca_cert, data={}, files={'file': ('ca.crt', 'SAMPLE_CA_CRT\n')})
-    print "Response" + r
+    print r
     r.raise_for_status()
     r = requests.post(url_server_cert, data={}, files={'file': ('server.crt', 'SAMPLE_CRT\n')})
-    print "Response" + r
+    print r
     r.raise_for_status()
     r = requests.post(url_server_key, data={}, files={'file': ('server.key', 'SAMPLE_KEY\n')})
-    print "Response" + r
+    print r
     r.raise_for_status()
     print "Configuration done"
 
     # build the vpn server
     print "Sending build request..."
     r = requests.post(url_build)
+    print r
     print "Build response: " + r.text
     r.raise_for_status()
     time.sleep(1)
     print "Sending start request..."
     r = requests.post(url_start)
+    print r
     print "Start response: " + r.text
     r.raise_for_status()
 
