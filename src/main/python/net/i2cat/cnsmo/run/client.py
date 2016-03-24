332ac7776cf3bd2cc93a9a1b528cf5edea0f3910
Isart Canyameres <isart.canyameres@i2cat.net>
2016-03-24 17:25:30 +0100

    Correct names in client runner.

--- src/main/python/net/i2cat/cnsmo/run/client.py
+++ src/main/python/net/i2cat/cnsmo/run/client.py
@@ -13,19 +13,19 @@ def get_app_request(host, port, service_id):
              dependencies=[],
 
              endpoints=[{"uri":"http://%s:%s/vpn/client/config/" %(host, port), "driver":"REST", "logic":"upload", "name":"set_config"},
-                        {"uri":"http://%s:%s/vpn/client/cert/ca/" %(host, port), "driver":"REST", "logic":"upload", "name":"set_ca"},
+                        {"uri":"http://%s:%s/vpn/client/cert/ca/" %(host, port), "driver":"REST", "logic":"upload", "name":"set_ca_cert"},
                         {"uri":"http://%s:%s/vpn/client/cert/" %(host, port), "driver":"REST", "logic":"upload", "name":"set_client_cert"},
                         {"uri":"http://%s:%s/vpn/client/key/" %(host, port),  "driver":"REST", "logic":"upload", "name":"set_client_key"},
                         {"uri":"http://%s:%s/vpn/client/build/" %(host, port), "driver":"REST", "logic":"post", "name":"build_client"},
-                        {"uri":"http://%s:%s/vpn/client/start/" %(host, port),  "driver":"REST", "logic":"post", "name":"start"},
-                        {"uri":"http://%s:%s/vpn/server/stop/" %(host, port), "driver":"REST", "logic":"post", "name":"stop"},])
+                        {"uri":"http://%s:%s/vpn/client/start/" %(host, port),  "driver":"REST", "logic":"post", "name":"start_client"},
+                        {"uri":"http://%s:%s/vpn/server/stop/" %(host, port), "driver":"REST", "logic":"post", "name":"stop_client"},])
     return d
 
 
 def main(host, port, redis_address, service_id):
 
     bash_deployer = BashDeployer(None)
-    configurer = CNSMOManager(redis_address, service_id, "ClientVPN", bash_deployer, None)
+    configurer = CNSMOManager(redis_address, service_id, "VPNClient", bash_deployer, None)
     configurer.start()
     configurer.compose_service(**get_app_request(host, port, service_id))
     configurer.launch_service(service_id)
@@ -42,8 +42,8 @@ if __name__ == "__main__":
 
     path = os.path.dirname(os.path.abspath(__file__))
     src_dir = path + "/../../../../../../../"
-    if not src_dir in sys.path:
-       sys.path.append(src_dir)
+    if src_dir not in sys.path:
+        sys.path.append(src_dir)
 
     from src.main.python.net.i2cat.cnsmo.deployment.bash import BashDeployer
     from src.main.python.net.i2cat.cnsmo.manager.cnsmo import CNSMOManager
