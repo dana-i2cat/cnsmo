9f1a1be4215d5d8bba403c7d0c135f2b7868513c
Isart Canyameres <isart.canyameres@i2cat.net>
2016-04-08 11:23:49 +0200

    Bash scripts leaving python running in background.

---
+++ src/main/python/net/i2cat/cnsmo/run/slipstream/vpn-server-deployment.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+python /tmp/cnsmo/cnsmo/src/main/python/net/i2cat/cnsmo/run/slipstream/vpn-server-deployment.py &
+disown $!
+ss-get --timeout=1800 net.i2cat.cnsmo.service.vpn.ready
