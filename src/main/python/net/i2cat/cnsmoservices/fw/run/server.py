f8e4df12dceb7cf0a1e26cd27ba52693b5f0f360
Isart Canyameres <isart.canyameres@i2cat.net>
2016-04-20 13:32:16 +0200

    Fix typo in resource url

--- src/main/python/net/i2cat/cnsmoservices/fw/run/server.py
+++ src/main/python/net/i2cat/cnsmoservices/fw/run/server.py
@@ -6,7 +6,7 @@ def get_server_app_request(host, port, service_id):
              trigger='python server.py -a %s -p %s' %(host, port),
              resources=["https://raw.githubusercontent.com/dana-i2cat/cnsmo/feature/fw-service/src/main/python/net/i2cat/cnsmoservices/fw/app/server.py",
                         "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/master/src/main/docker/fw/Dockerfile",
-                        "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/master/src/main/docker/fw/sc_manager.py",],
+                        "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/master/src/main/docker/fw/sc-manager.py",],
              dependencies=[],
              endpoints=[{ "uri":"http://%s:%s/fw/" %(host, port), "driver":"REST", "logic":"post", "name":"add_rule"},
                         { "uri":"http://%s:%s/fw/" %(host, port), "driver":"REST", "logic":"delete", "name":"delete_rule"},])
