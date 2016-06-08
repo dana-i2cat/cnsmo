72aafdca8700ebf9bbf005715e2b04d36cf8c10c
Isart Canyameres <isart.canyameres@i2cat.net>
2016-06-08 17:23:37 +0200

    USE TEMPORARY BRANCH

--- src/main/python/net/i2cat/cnsmoservices/lb/run/server.py
+++ src/main/python/net/i2cat/cnsmoservices/lb/run/server.py
@@ -16,7 +16,7 @@ def get_server_app_request(host, port, service_id, lb_port):
 
     d = dict(service_id=service_id,
              trigger='python server.py -a %s -p %s -t %s -w "$(pwd)"' %(host, port, lb_port),
-             resources=["https://raw.githubusercontent.com/dana-i2cat/cnsmo/master/src/main/python/net/i2cat/cnsmoservices/lb/app/server.py",
+             resources=["https://raw.githubusercontent.com/dana-i2cat/cnsmo/feature/lb-service/src/main/python/net/i2cat/cnsmoservices/lb/app/server.py",
                         "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/master/src/main/docker/lb/start.bash",],
              dependencies=[],
              endpoints=[{"uri":"http://%s:%s/lb/server/config/" %(host, port), "driver":"REST", "logic":"upload",
