71e13de3092525f7c34af94ce6214f447eb4c52a
Isart Canyameres <isart.canyameres@i2cat.net>
2016-06-09 13:11:14 +0200

    Update url.service with LB address

--- src/main/python/net/i2cat/cnsmoservices/lb/run/slipstream/lb-orchestrator-deployment.sh
+++ src/main/python/net/i2cat/cnsmoservices/lb/run/slipstream/lb-orchestrator-deployment.sh
@@ -4,3 +4,10 @@ cwd=${PWD}
 python ${cwd}/cnsmo/cnsmo/src/main/python/net/i2cat/cnsmoservices/lb/run/slipstream/lb-orchestrator-deployment.py &
 disown $!
 ss-get --timeout=1800 net.i2cat.cnsmo.service.lb.ready
+
+address=$(ss-get hostname)
+port=$(ss-get lb.port)
+ss-set url.service ${address}:${port}
+
+# Set deployment global url.service
+ss-set ss:url.service ${address}:${port}
