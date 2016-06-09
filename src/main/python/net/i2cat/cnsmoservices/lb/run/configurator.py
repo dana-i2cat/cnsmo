59440bc184d635fd4645057cf33aac1a9cce65d6
Isart Canyameres <isart.canyameres@i2cat.net>
2016-06-09 13:22:25 +0200

    Revert "USE TEMPORARY BRANCH"
    This reverts commit 72aafdca8700ebf9bbf005715e2b04d36cf8c10c.

--- src/main/python/net/i2cat/cnsmoservices/lb/run/configurator.py
+++ src/main/python/net/i2cat/cnsmoservices/lb/run/configurator.py
@@ -18,7 +18,7 @@ def get_app_request(host, port, service_id, lb_address, lb_port, lb_mode, lb_bac
 
              trigger= 'python configurator.py -a %s -p %s -s %s -t %s -m %s -b %s' %(host, port, lb_address, lb_port, lb_mode, lb_backend_servers),
 
-             resources = ["https://raw.githubusercontent.com/dana-i2cat/cnsmo/feature/lb-service/src/main/python/net/i2cat/cnsmoservices/lb/app/configurator.py",
+             resources = ["https://raw.githubusercontent.com/dana-i2cat/cnsmo/master/src/main/python/net/i2cat/cnsmoservices/lb/app/configurator.py",
                           "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/master/src/main/docker/lb/start.bash",
                           ],
              dependencies=[],
