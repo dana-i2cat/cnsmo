aab6059ba55036e2bca86faaef645a11d66c3698
Isart Canyameres <isart.canyameres@i2cat.net>
2016-08-25 17:45:33 +0200

    Don't build new-easy-rsa, a prebuild container is being used now.

--- src/main/python/net/i2cat/cnsmoservices/vpn/run/slipstream/vpnserverpostinstall.sh
+++ src/main/python/net/i2cat/cnsmoservices/vpn/run/slipstream/vpnserverpostinstall.sh
@@ -1,32 +1,59 @@
 #!/bin/bash
 
-if [ $(docker --version 1>/dev/null 2>/dev/null; echo $?) != "0" ] ; then
+safeRunCommand() {
+  typeset cmnd="$*"
+  typeset ret_code
+
+  echo ${cmnd}
+  eval ${cmnd}
+  ret_code=$?
+  if [ ${ret_code} != 0 ]; then
+    printf "Error executing command: '$cmnd'"
+    exit ${ret_code}
+  fi
+}
+
+
+if [ $(docker --version 1>/dev/null 2>/dev/null; echo $?) != "0" ]; then
     # install docker
+    prinf "installing docker..."
     curl -fsSL https://get.docker.com/ | sh
     current_user=$(whoami)
     usermod -aG docker ${current_user}
+    prinf "installing docker... done!"
 else
-    echo "docker already installed"
+    prinf "docker already installed"
 fi
 
+
 # download cnsmo
+prinf "downloading cnsmo..."
 mkdir cnsmo
 cd cnsmo
 git clone --single-branch https://github.com/dana-i2cat/cnsmo.git
 git clone --single-branch https://github.com/dana-i2cat/cnsmo-net-services.git
 cd ..
+prinf "downloading cnsmo... done!"
+
 
 # install cnsmo requirements
+prinf "installing cnsmo..."
 pip install -r cnsmo/cnsmo/requirements.txt
+prinf "installing cnsmo... done!"
 
 
-#build new-easy-rsa docker
-cwd=${PWD}
-cd ${cwd}/cnsmo/cnsmo-net-services/src/main/docker/vpn/easy-rsa
-docker build -t new-easy-rsa .
-cd ${cwd}
+# build new-easy-rsa docker
+# prinf "building easy-rsa docker container..."
+# cwd=${PWD}
+# cd ${cwd}/cnsmo/cnsmo-net-services/src/main/docker/vpn/easy-rsa
+# cmnd="docker build -t new-easy-rsa ."
+# safeRunCommand "$cmnd"
+# cd ${cwd}
+# prinf "building easy-rsa docker container... done!"
+
 
 #install redis
+prinf "installing redis..."
 wget http://download.redis.io/releases/redis-3.0.7.tar.gz
 tar xzf redis-3.0.7.tar.gz
 rm redis-3.0.7.tar.gz
@@ -34,3 +61,4 @@ cd redis-3.0.7
 make
 make install --quiet
 cd ..
+prinf "installing redis... done!"
