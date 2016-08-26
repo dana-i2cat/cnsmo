6bbcd4e976eec2cf9b30bc148d82206746d567dd
Isart Canyameres <isart.canyameres@i2cat.net>
2016-08-26 11:33:25 +0200

    Clean up

--- src/main/python/net/i2cat/cnsmoservices/vpn/run/slipstream/vpnserverpostinstall.sh
+++ src/main/python/net/i2cat/cnsmoservices/vpn/run/slipstream/vpnserverpostinstall.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-safeRunCommand() {
-  typeset cmnd="$*"
-  typeset ret_code
-
-  echo ${cmnd}
-  eval ${cmnd}
-  ret_code=$?
-  if [ ${ret_code} != 0 ]; then
-    printf "Error executing command: '$cmnd'"
-    exit ${ret_code}
-  fi
-}
-
-
 if [ $(docker --version 1>/dev/null 2>/dev/null; echo $?) != "0" ]; then
     # install docker
     prinf "installing docker..."
@@ -40,16 +26,6 @@ prinf "downloading cnsmo... done!"
 prinf "installing cnsmo..."
 pip install -r cnsmo/cnsmo/requirements.txt
 prinf "installing cnsmo... done!"
-
-
-# build new-easy-rsa docker
-# prinf "building easy-rsa docker container..."
-# cwd=${PWD}
-# cd ${cwd}/cnsmo/cnsmo-net-services/src/main/docker/vpn/easy-rsa
-# cmnd="docker build -t new-easy-rsa ."
-# safeRunCommand "$cmnd"
-# cd ${cwd}
-# prinf "building easy-rsa docker container... done!"
 
 
 #install redis
