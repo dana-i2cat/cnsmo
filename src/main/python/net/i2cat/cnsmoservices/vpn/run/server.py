aedec6b84f192b47a16401669f457fc7fcbda99b
Isart Canyameres <isart.canyameres@i2cat.net>
2016-09-05 09:54:57 +0200

    Revert "TO REVERT: Use temporary branch"
    This reverts commit cdf9a2831a05f55496c3bf9cf5e9936b3e8ed8ff.

--- src/main/python/net/i2cat/cnsmoservices/vpn/run/server.py
+++ src/main/python/net/i2cat/cnsmoservices/vpn/run/server.py
@@ -6,7 +6,7 @@ def get_server_app_request(host, port, service_id):
 
     d = dict(service_id=service_id,
              trigger='python server.py -a %s -p %s -w "$(pwd)"' %(bind_address, port),
-             resources=["https://raw.githubusercontent.com/dana-i2cat/cnsmo/feature/use-ports-20000-25000/src/main/python/net/i2cat/cnsmoservices/vpn/app/server.py",
+             resources=["https://raw.githubusercontent.com/dana-i2cat/cnsmo/master/src/main/python/net/i2cat/cnsmoservices/vpn/app/server.py",
                         "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/master/src/main/docker/vpn/server/Dockerfile",
                         "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/master/src/main/docker/vpn/server/tun_manager.sh",],
              dependencies=[],
