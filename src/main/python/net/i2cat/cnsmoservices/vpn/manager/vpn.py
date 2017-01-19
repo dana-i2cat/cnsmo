b9d5924a4a72c6fc58367dd38db14aa270c8de93
Albert <albert.vines@gmail.com>
2017-01-19 10:11:33 +0100

    Removed commented func

--- src/main/python/net/i2cat/cnsmoservices/vpn/manager/vpn.py
+++ src/main/python/net/i2cat/cnsmoservices/vpn/manager/vpn.py
@@ -162,57 +162,6 @@ class VPNManager:
         self.__configure_and_start_vpn_client(client_service, client_id, ca_crt, client_key, client_crt, client_conf)
 
 
-    # def __deploy_vpn(self):
-    #     """
-    #     Main service of the VPN orchestrator. Here is the logic of the VPN manager, this method is called after start()
-    #     successfully works.
-    #
-    #     The idea is to deploy all the VPN instances all over the context and manage them. For the Poc, we only read the
-    #     two server strings provided by the two deployed apps
-    #     :return:
-    #     """
-    #     self.__logger.debug("Deploying VPN...")
-    #     print "Deploying VPN..."
-    #
-    #     self.__logger.debug("generating security mechanism...")
-    #     print "generating security mechanism..."
-    #
-    #     # Generate DH and CA cert
-    #     self.__configuration_manager.generate_ca_cert(None)
-    #
-    #     self.__logger.debug("generating vpn server configuration...")
-    #     print "generating vpn server configuration..."
-    #     # Generate server key and cert
-    #     self.__configuration_manager.generate_server_cert(None)
-    #
-    #     # Get all config files
-    #     dh = self.__configuration_manager.get_dh(None).content
-    #     ca_crt = self.__configuration_manager.get_ca_cert(None).content
-    #     server_key = self.__configuration_manager.get_server_key(None).content
-    #     server_crt = self.__configuration_manager.get_server_cert(None).content
-    #     server_conf = self.__configuration_manager.get_server_config(None).content
-    #
-    #     self.__configure_and_start_vpn_server("server", dh, ca_crt, server_key, server_crt, server_conf)
-    #
-    #     # for each client:
-    #     # Generate client config, get it and configure the client service
-    #     i = 0
-    #     for client_service in self.__client_services:
-    #         client_id = "client-" + str(i)
-    #
-    #         self.__logger.debug("generating vpn client configuration...")
-    #         print "generating vpn client configuration..."
-    #         self.__configuration_manager.generate_client_cert(client_id, None)
-    #         client_key = self.__configuration_manager.get_client_key(client_id).content
-    #         client_crt = self.__configuration_manager.get_client_cert(client_id).content
-    #         client_conf = self.__configuration_manager.get_client_config(client_id).content
-    #
-    #         self.__configure_and_start_vpn_client(client_service, client_id, ca_crt, client_key, client_crt, client_conf)
-    #         i += 1
-    #
-    #     self.__logger.debug("VPN deployed.")
-    #     print "VPN deployed."
-
     def __configure_and_start_vpn_server(self, name, dh, ca_crt, server_key, server_crt, server_conf):
         """
         Helper method that configures server service with given configuration and starts the service
