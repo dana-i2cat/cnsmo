06ce3599a8ba4d44cb92c9995b19b85fb30a2e78
Isart Canyameres <isart.canyameres@i2cat.net>
2017-01-31 18:28:31 +0100

    Make system state client stoppable.

--- src/main/python/net/i2cat/cnsmo/system/state/driver/redis/listener.py
+++ src/main/python/net/i2cat/cnsmo/system/state/driver/redis/listener.py
@@ -12,6 +12,7 @@ class RedisListener(threading.Thread):
         """
         threading.Thread.__init__(self)
         print redis_address
+        self.__should_stop = False
         self.__stream = redis.Redis(redis_address.split(":")[0], int(redis_address.split(":")[1])).pubsub()
 
     def subscribe(self, channel, callback):
@@ -26,8 +27,11 @@ class RedisListener(threading.Thread):
     def unsubscribe(self, channel):
         self.__stream.unsubscribe(channel)
 
+    def stop(self):
+        self.__should_stop = True
+
     def run(self):
         for item in self.__stream.listen():
             #This is only blocking the thread, messages will be handled by the provided callback
-            pass
-
+            if self.__should_stop:
+                break
