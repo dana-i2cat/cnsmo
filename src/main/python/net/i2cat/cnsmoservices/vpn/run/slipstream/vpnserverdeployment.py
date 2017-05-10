fc363b31b79bae4774de60509e5c11eccdf79420
josep pons <josep.pons@i2cat.net>
2017-05-10 17:49:36 +0200

    adding logs 4

--- src/main/python/net/i2cat/cnsmoservices/vpn/run/slipstream/vpnserverdeployment.py
+++ src/main/python/net/i2cat/cnsmoservices/vpn/run/slipstream/vpnserverdeployment.py
@@ -139,13 +139,13 @@ def deployvpn():
     vpn_local_ipv4_address = getInterfaceIPv4Address(vpn_iface)
     vpn_local_ipv6_address = "" #JOSEP CODE
     #vpn_local_ipv6_address = getInterfaceIPv6Address(vpn_iface)    #JOSEP COMENTED
-    call('ss-display \"interface %s\"' % vpn_iface)
     logger.debug("VPN using interface %s with ipaddr %s and ipv6addr %s"
+                 % (vpn_iface, vpn_local_ipv4_address, vpn_local_ipv6_address))
+    print "VPN using interface %s with ipaddr %s and ipv6addr %s"
                  % (vpn_iface, vpn_local_ipv4_address, vpn_local_ipv6_address))
 
     logger.debug("Announcing IP addresses...")
     call('ss-display \"VPN: Announcing IP addresses...\"')
-    call('ss-display \"address %s\"' % vpn_local_ipv4_address)
     call("ss-set vpn.address %s" % vpn_local_ipv4_address)
     call("ss-set vpn.address6 %s" % vpn_local_ipv6_address)
 
