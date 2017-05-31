eea039b872e301fcef07c787b4be03407f7c2ede
PaulAlmP <paul.almasan@i2cat.net>
2017-05-31 09:16:20 +0100

    Deleted files manually

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnclientpostinstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Run this script using sudo
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit 126
-fi
-
-printf "Installing OpenvSwitch..." 
-apt-get install -y openvswitch-switch
