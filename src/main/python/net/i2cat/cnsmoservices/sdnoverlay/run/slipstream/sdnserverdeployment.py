61300ce2ffec1db6b9ba737a02c9bee71b7fb777
PaulAlmP <paul.almasan@i2cat.net>
2017-06-01 14:00:41 +0100

    develop changed by feature/sdn

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverdeployment.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverdeployment.py
@@ -34,41 +34,19 @@ def deploysdn():
     hostname = call('ss-get hostname').rstrip('\n')
     log_file = os.getcwd() + "/cnsmo/sdn.log"
 
-    call('ss-set sdn.server.nodeinstanceid %s' % instance_id)
-    logger.debug("Set sdn.server.nodeinstanceid= %s" % instance_id)
+    call('ss-set vpn.server.nodeinstanceid %s' % instance_id)
+    logger.debug("Set vpn.server.nodeinstanceid= %s" % instance_id)
 
     # Launch SDN server
     logger.debug("Assuming SDN server is listening")
-    call('ss-set net.i2cat.cnsmo.service.sdn.server.listening true')
+    call('ss-set net.i2cat.cnsmo.service.vpn.server.listening true')
 
     # Wait for configurator and server to be ready
-    call('ss-get net.i2cat.cnsmo.service.sdn.server.listening')
+    call('ss-get net.i2cat.cnsmo.service.vpn.server.listening')
 
-    ##### Wait for clients  [required for a correct working when join scenario with load balancer service]
-    logger.debug("Detecting all SDN clients...")
-    call('ss-display \"SDN: Looking for all clients...\"')
-    # All instances in the deployment are considered sdn clients
-    # except the slipstream orchestrator and the one running the sdn server
-    all_instances = ss_getinstances()
-    # remove slipstream orchestrator instances
-    client_instances = [x for x in all_instances if not x.startswith("orchestrator")]
-    # remove this instance
-    client_instances.remove(instance_id)
-    logger.debug("Finished detecting all SDN clients: %s" % client_instances)
-    logger.debug("Waiting for all SDN clients...")
-    call('ss-display \"SDN: Waiting for all clients...\"')
-    # wait for clients to be ready: instance_id:net.i2cat.cnsmo.service.sdn.client.waiting=true
-    for client_id in client_instances:
-        logger.debug("Waiting for SDN client %s" % client_id)
-        response = call("ss-get --timeout=1800 %s:net.i2cat.cnsmo.service.sdn.client.listening" % client_id).rstrip("\n")
-        if not response:
-            logger.error("Timeout! Waiting for SDN client %s" % client_id)
-            return -1
-        logger.debug("Finished waiting for all SDN clients.")
-
-    # Communicate that the VPN has been established
+    # Communicate that the SDN has been established
     logger.debug("Announcing sdn service has been deployed")
-    call('ss-set net.i2cat.cnsmo.service.sdn.ready true')
+    call('ss-set net.i2cat.cnsmo.service.vpn.ready true')
     logger.debug("Set net.i2cat.cnsmo.service.sdn.ready=true")
     return 0
 
