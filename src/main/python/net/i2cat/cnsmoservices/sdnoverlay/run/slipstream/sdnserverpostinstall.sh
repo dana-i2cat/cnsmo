39aadd6cf540445d5b31e932eab69b0566310a19
PaulAlmP <paul.almasan@i2cat.net>
2017-06-08 07:55:38 +0100

    SDN condition added

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.sh
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.sh
@@ -12,28 +12,30 @@ if [ ! -d "$DIRECTORY" ]; then
 fi
 cd ${DIRECTORY}
 
-touch /var/log/sdnserverinstall.log
+services=$(ss-get net.services.enable)
+if 'sdn' in services:
+  touch /var/log/sdnserverinstall.log
 
-echo "Installing Java 7 JDK and other components..." >> /var/log/sdnserverinstall.log
-apt-get -y update
-apt-get install -y openjdk-7-jdk 
+  echo "Installing Java 7 JDK and other components..." >> /var/log/sdnserverinstall.log
+  apt-get -y update
+  apt-get install -y openjdk-7-jdk 
 
-# download opendaylight
-echo "Downloading opendaylight executable..." >> /var/log/sdnserverinstall.log
-wget https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.3.2-Lithium-SR2/distribution-karaf-0.3.2-Lithium-SR2.zip
-unzip distribution-karaf-0.3.2-Lithium-SR2.zip
-cd distribution-karaf-0.3.2-Lithium-SR2
+  # download opendaylight
+  echo "Downloading opendaylight executable..." >> /var/log/sdnserverinstall.log
+  wget https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.3.2-Lithium-SR2/distribution-karaf-0.3.2-Lithium-SR2.zip
+  unzip distribution-karaf-0.3.2-Lithium-SR2.zip
+  cd distribution-karaf-0.3.2-Lithium-SR2
 
-echo 'export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"' >> ./bin/setenv
-source ./bin/setenv
-# start karaf server
-echo "Starting karaf server..." >> /var/log/sdnserverinstall.log
-#./bin/karaf server &
-./bin/start
+  echo 'export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"' >> ./bin/setenv
+  source ./bin/setenv
+  # start karaf server
+  echo "Starting karaf server..." >> /var/log/sdnserverinstall.log
+  #./bin/karaf server &
+  ./bin/start
 
-# install features
-echo "Installing karaf features..." >> /var/log/sdnserverinstall.log
-sleep 30
+  # install features
+  echo "Installing karaf features..." >> /var/log/sdnserverinstall.log
+  sleep 30
 
-./bin/client -u karaf feature:install odl-openflowjava-all odl-netconf-all odl-dlux-all odl-l2switch-packethandler odl-l2switch-loopremover odl-l2switch-arphandler odl-l2switch-switch-ui odl-restconf-all odl-l2switch-addresstracker odl-l2switch-switch-rest odl-l2switch-switch odl-mdsal-all odl-openflowjava-all odl-mdsal-apidocs odl-openflowplugin-all odl-ovsdb-all
-echo "Karaf features installed successfully and ready to run!" >> /var/log/sdnserverinstall.log
+  ./bin/client -u karaf feature:install odl-openflowjava-all odl-netconf-all odl-dlux-all odl-l2switch-packethandler odl-l2switch-loopremover odl-l2switch-arphandler odl-l2switch-switch-ui odl-restconf-all odl-l2switch-addresstracker odl-l2switch-switch-rest odl-l2switch-switch odl-mdsal-all odl-openflowjava-all odl-mdsal-apidocs odl-openflowplugin-all odl-ovsdb-all
+  echo "Karaf features installed successfully and ready to run!" >> /var/log/sdnserverinstall.log
