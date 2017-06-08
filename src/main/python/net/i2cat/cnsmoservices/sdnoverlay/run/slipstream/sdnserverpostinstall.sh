224a7b88f1ef6c51d49d42eb04c4c12daed9907b
PaulAlmP <paul.almasan@i2cat.net>
2017-06-08 09:37:07 +0100

    If condition sdn solved

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.sh
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.sh
@@ -13,8 +13,8 @@ fi
 cd ${DIRECTORY}
 
 services=$(ss-get net.services.enable)
-echo services >> /var/log/sdnserverinstall.log
-if 'sdn' in services:
+echo $services >> /var/log/sdnserverinstall.log
+if ['sdn' in services]; then
   touch /var/log/sdnserverinstall.log
 
   echo "Installing Java 7 JDK and other components..." >> /var/log/sdnserverinstall.log
@@ -40,3 +40,4 @@ if 'sdn' in services:
 
   ./bin/client -u karaf feature:install odl-openflowjava-all odl-netconf-all odl-dlux-all odl-l2switch-packethandler odl-l2switch-loopremover odl-l2switch-arphandler odl-l2switch-switch-ui odl-restconf-all odl-l2switch-addresstracker odl-l2switch-switch-rest odl-l2switch-switch odl-mdsal-all odl-openflowjava-all odl-mdsal-apidocs odl-openflowplugin-all odl-ovsdb-all
   echo "Karaf features installed successfully and ready to run!" >> /var/log/sdnserverinstall.log
+fi
