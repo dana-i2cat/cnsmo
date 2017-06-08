e546a4da609b04531c66ea37c458702be6864ea1
PaulAlmP <paul.almasan@i2cat.net>
2017-06-08 07:49:20 +0100

    SDN condition added

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverdeployment.sh
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverdeployment.sh
@@ -4,7 +4,9 @@
 DIRECTORY='/var/tmp/slipstream'
 cd ${DIRECTORY}
 
-cwd=${PWD}
-python ${cwd}/cnsmo/cnsmo/src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverdeployment.py &
-disown $!
-ss-get --timeout=1800 net.i2cat.cnsmo.service.sdn.server.ready
+services=$(ss-get net.services.enable)
+if 'sdn' in services:
+    cwd=${PWD}
+    python ${cwd}/cnsmo/cnsmo/src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverdeployment.py &
+    disown $!
+    ss-get --timeout=1800 net.i2cat.cnsmo.service.sdn.server.ready
