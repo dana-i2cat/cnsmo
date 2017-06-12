dcfdea29c67f4d28920d30556871d4b3d561bbc0
PaulAlmP <paul.almasan@i2cat.net>
2017-06-12 10:50:39 +0100

    cnsmo-net-services/develop changed to master

--- src/test/python/cnsmoservices/vpn/client.py
+++ src/test/python/cnsmoservices/vpn/client.py
@@ -9,8 +9,8 @@ def get_app_request():
              trigger='python client.py -a 127.0.0.1 -p 9092 -w "$(pwd)"',
 
              resources=["https://raw.githubusercontent.com/dana-i2cat/cnsmo/SDNdevelop/src/main/python/net/i2cat/cnsmoservices/vpn/app/client.py",
-                        "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/develop/src/main/docker/vpn/client/Dockerfile",
-                        "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/develop/src/main/docker/vpn/client/tun_manager.sh",
+                        "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/master/src/main/docker/vpn/client/Dockerfile",
+                        "https://raw.githubusercontent.com/dana-i2cat/cnsmo-net-services/master/src/main/docker/vpn/client/tun_manager.sh",
                         ],
 
              dependencies=[],
