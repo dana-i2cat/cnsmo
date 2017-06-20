b2b05e4178f748347862ea2d33fa41746365233c
PaulAlmP <paul.almasan@i2cat.net>
2017-06-20 07:19:21 +0100

    Return of karaf function added

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.py
@@ -68,6 +68,7 @@ def install_karaf():
 
     p = subprocess.Popen(["./bin/client","-u","karaf","feature:install","odl-openflowjava-all","odl-netconf-all","odl-dlux-all","odl-l2switch-packethandler","odl-l2switch-loopremover","odl-l2switch-arphandler","odl-l2switch-switch-ui","odl-restconf-all","odl-l2switch-addresstracker","odl-l2switch-switch-rest","odl-l2switch-switch","odl-mdsal-all","odl-openflowjava-all","odl-mdsal-apidocs","odl-openflowplugin-all","odl-ovsdb-all"])    
     logger.debug("Karaf features installed successfully and ready to run!")
+    return p
 
 def postinstallsdn():
     logger = logging.getLogger(__name__)
