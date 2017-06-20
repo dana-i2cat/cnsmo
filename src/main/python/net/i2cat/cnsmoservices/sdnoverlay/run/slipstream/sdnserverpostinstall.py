d07e6058d9266089b211d1af6e8d14c9b53106ef
PaulAlmP <paul.almasan@i2cat.net>
2017-06-20 07:37:04 +0100

    Wait instance added

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.py
@@ -67,6 +67,7 @@ def install_karaf():
     time.sleep(30)
 
     p = subprocess.Popen(["./bin/client","-u","karaf","feature:install","odl-openflowjava-all","odl-netconf-all","odl-dlux-all","odl-l2switch-packethandler","odl-l2switch-loopremover","odl-l2switch-arphandler","odl-l2switch-switch-ui","odl-restconf-all","odl-l2switch-addresstracker","odl-l2switch-switch-rest","odl-l2switch-switch","odl-mdsal-all","odl-openflowjava-all","odl-mdsal-apidocs","odl-openflowplugin-all","odl-ovsdb-all"])    
+    p.wait()
     logger.debug("Karaf features installed successfully and ready to run!")
     return p
 
