d58f35ad0789165f3fb714c69d879a2685ae7a5c
PaulAlmP <paul.almasan@i2cat.net>
2017-06-20 10:07:18 +0100

    Waitpid added

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.py
@@ -67,7 +67,8 @@ def install_karaf():
     logger.debug("Installing karaf features")
     time.sleep(30)
 
-    p = subprocess.Popen(["./bin/client","-u","karaf","feature:install","odl-openflowjava-all","odl-netconf-all","odl-dlux-all","odl-l2switch-packethandler","odl-l2switch-loopremover","odl-l2switch-arphandler","odl-l2switch-switch-ui","odl-restconf-all","odl-l2switch-addresstracker","odl-l2switch-switch-rest","odl-l2switch-switch","odl-mdsal-all","odl-openflowjava-all","odl-mdsal-apidocs","odl-openflowplugin-all","odl-ovsdb-all"])    
+    p = subprocess.Popen(["./bin/client","-u","karaf","feature:install","odl-openflowjava-all","odl-netconf-all","odl-dlux-all","odl-l2switch-packethandler","odl-l2switch-loopremover","odl-l2switch-arphandler","odl-l2switch-switch-ui","odl-restconf-all","odl-l2switch-addresstracker","odl-l2switch-switch-rest","odl-l2switch-switch","odl-mdsal-all","odl-openflowjava-all","odl-mdsal-apidocs","odl-openflowplugin-all","odl-ovsdb-all"], stdout=PIPE)    
+    os.waitpid(p.pid,0)
     logger.debug("Karaf features installed successfully and ready to run!")
     return p
 
