6ace5f831ab0ea2438e0f9e952a26a92d9140923
PaulAlmP <paul.almasan@i2cat.net>
2017-06-23 10:40:09 +0100

    Comments added for documentation phase

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnclientdeployment.sh
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnclientdeployment.sh
@@ -9,7 +9,6 @@ python ${cwd}/cnsmo/cnsmo/src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run
 disown $!
 serverid=$(ss-get --timeout=1200 vpn.server.nodeinstanceid)
 # In case of timeout, serverid will not be set. But next ss-get will fail with Unknown key error.
-# Better to finish script immediately (ss-get that got timeout will abort the deployment in short time)
 if [[ -z "$serverid" ]]; then
 exit 1
 else
