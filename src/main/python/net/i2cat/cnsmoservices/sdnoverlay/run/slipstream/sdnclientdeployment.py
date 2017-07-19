412bc2d873283c30187e341d555dd52115f200fa
PaulAlmP <paul.almasan@i2cat.net>
2017-07-19 12:53:34 +0100

    More comments of SDN variables

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnclientdeployment.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnclientdeployment.py
@@ -43,13 +43,13 @@ def check_error(err):
 def check_preconditions(sdn_server_instance_id):
     logger = logging.getLogger(__name__)
 
-    logger.debug("Resolving net.i2cat.cnsmo.service.sdn.allowedip...")
-    allowed_ip_and_mask = callWithResp('ss-get --timeout=1200 net.i2cat.cnsmo.service.sdn.allowedip').rstrip('\n')
-    if not allowed_ip_and_mask:
-        logger.error("Timeout waiting for net.i2cat.cnsmo.service.sdn.allowedip")
+    #logger.debug("Resolving net.i2cat.cnsmo.service.sdn.allowedip...")
+    #allowed_ip_and_mask = callWithResp('ss-get --timeout=1200 net.i2cat.cnsmo.service.sdn.allowedip').rstrip('\n')
+    #if not allowed_ip_and_mask:
+        #logger.error("Timeout waiting for net.i2cat.cnsmo.service.sdn.allowedip")
         #timeout! Abort the script immediately (ss-get will abort the whole deployment in short time)
         return -1
-    logger.debug("Got net.i2cat.cnsmo.service.sdn.allowedip= %s" % allowed_ip_and_mask)
+    #logger.debug("Got net.i2cat.cnsmo.service.sdn.allowedip= %s" % allowed_ip_and_mask)
 
     logger.debug("Resolving net.i2cat.cnsmo.service.sdn.allowedport...")
     allowed_port = callWithResp('ss-get --timeout=1200 net.i2cat.cnsmo.service.sdn.allowedport').rstrip('\n')
