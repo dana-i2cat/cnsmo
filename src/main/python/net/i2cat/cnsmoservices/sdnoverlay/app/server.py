4fd1715eddd5657d40ebf843a694b5c05793db51
PaulAlmP <paul.almasan@i2cat.net>
2017-08-02 10:09:08 +0100

    Asdasdasd

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/app/server.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/app/server.py
@@ -36,7 +36,7 @@ def get_nodes():
     nodes = {}
     for key in j['nodes']['node']:
         nodes[str(key['id'])] = str(key['flow-node-inventory:ip-address'])
-        
+
     return jsonify(nodes),200
 
 #la crida sera del format: /blockbyport/SlipstreamInstanceId:port
@@ -95,7 +95,7 @@ def get_corresp_vpn(ssinstanceid):
 
 def get_flowID(vpnaddress):
     nodes = requests.get('http://127.0.0.1:20199/sdn/server/nodes/')
-    nodes = nodes.json()
+    nodes = jsonify(nodes.headers)
     for key,value in nodes.iteritems():
         if str(value)==vpnaddress:
             return str(key)
