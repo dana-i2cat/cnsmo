b0956fd4b030fdc4ab4b0a053228a88d6523f6c6
PaulAlmP <paul.almasan@i2cat.net>
2017-08-09 11:43:19 +0100

    Read parameters from body. need to do tests

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/app/server.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/app/server.py
@@ -52,7 +52,6 @@ def add_filter_by_port(ssinstanceid):
         if flowID!="":
             # URL has to follow this format: http://134.158.74.110:8080/restconf/config/opendaylight-inventory:nodes/node/openflow:274973442922995/table/0/flow/12
             url = str("http://134.158.74.110:8080/restconf/config/opendaylight-inventory:nodes/node/"+flowID+"/table/0/flow/"+str(newflowCount))
-            data = request.json
             call("echo %s >> /var/tmp/sdntest.txt" % data["tcp-destination-port"])
             xml = """
             <flow xmlns="urn:opendaylight:flow:inventory">
