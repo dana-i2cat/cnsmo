61267c9e9c9aebeef2bfa4dc194842812c0f6724
PaulAlmP <paul.almasan@i2cat.net>
2017-08-21 09:16:22 +0100

    Data json removed

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/app/server.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/app/server.py
@@ -86,7 +86,6 @@ def add_filter_by_port(ssinstanceid):
         if flowID!="":
             # URL has to follow this format: http://134.158.74.110:8080/restconf/config/opendaylight-inventory:nodes/node/openflow:274973442922995/table/0/flow/12
             url = str("http://134.158.74.110:8080/restconf/config/opendaylight-inventory:nodes/node/"+flowID+"/table/0/flow/"+str(newflowCount))
-            data = request.json
             xml = """
             <flow xmlns="urn:opendaylight:flow:inventory">
                 <strict>false</strict>
