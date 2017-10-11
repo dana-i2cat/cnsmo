0501463927373fe91ca2c7cca6616a89e2d2c47b
josep pons <josep.pons@i2cat.net>
2017-10-11 12:40:13 +0200

    adding scripts for instantiating cnsmo services integrated version

--- src/main/python/net/i2cat/cnsmoservices/integrated/run/slipstream/netservicesclientdeployment.sh
+++ src/main/python/net/i2cat/cnsmoservices/integrated/run/slipstream/netservicesclientdeployment.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# set working directory
-DIRECTORY='/var/tmp/slipstream'
-cd ${DIRECTORY}
+wd='/var/tmp/slipstream'
+cd ${wd}
+
+cwd=${PWD}
+python ${cwd}/cnsmo/cnsmo/src/main/python/net/i2cat/cnsmo/run/slipstream/cnsmo-deployment.py &
+disown $!
+ss-get --timeout=1800 net.i2cat.cnsmo.core.ready
+
 
 cwd=${PWD}
 python ${cwd}/cnsmo/cnsmo/src/main/python/net/i2cat/cnsmoservices/integrated/run/slipstream/netservicesclientdeployment.py &
 disown $!
-ss-get --timeout=1800 net.services.enabled
+ss-get --timeout=3600 net.services.enabled
