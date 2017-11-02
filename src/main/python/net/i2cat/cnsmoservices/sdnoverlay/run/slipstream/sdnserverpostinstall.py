3495b1b5327359cc7d00e4cba81e9595adc14b20
josep pons <josep.pons@i2cat.net>
2017-11-02 16:43:00 +0100

    putting again gui_installation inside sdnserverpostinstall.py

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/run/slipstream/sdnserverpostinstall.py
@@ -52,9 +52,52 @@ def install_karaf():
     logger.debug("Set net.services.installed=true")
     return 0
 
+def install_gui():
+    os.chdir("/var/tmp")
+
+    call("echo fase 1 - clone api node project... >> /var/tmp/slipstream/cnsmo_api_gui.log")
+    call("curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -")
+    call("sudo apt-get install -y nodejs")
+    call("git clone https://github.com/jiponsI2cat/cnsmo-api.git")
+    call("echo fase 2 - install api node dependencies... >> /var/tmp/slipstream/cnsmo_api_gui.log")
+    os.chdir("/var/tmp/cnsmo-api")
+    call("npm install")
+    PORT = "8081"
+    BASE_URL = "/api/v1"
+    call("echo '\"use strict\";\n\nmodule.exports = {\n  BASE_URL: \""+BASE_URL+"\",\n  MONGO_URL: \"mongodb://localhost/cnsmo\",\n  DOMAIN: \"127.0.0.1\",\n  PROTOCOL: \"http\",\n  port: "+PORT+",\n  SWAGGER: true,\n  JWT_SECRET: \"cnsmosecret\",\n  TOKEN_EXPIRATION_DAYS: 10,\n};' > config/env/production.js")
+    
+    call("echo fase 3 - retrive host ip and generate environment.prod.ts file... >> /var/tmp/slipstream/cnsmo_api_gui.log")
+    os.chdir("/var/tmp/cnsmo-api/node_modules/cnsmo_web/src/environments")
+    IPADDR = call('ss-get hostname').rstrip('\n')
+    aux = "http://"+str(IPADDR)+":"+PORT+BASE_URL
+    call("echo 'export const environment = {\n production: true,\n api: \"%s\",\n authUrl: \"/authenticate\"\n };' > environment.prod.ts " % aux)
+    os.chdir("/var/tmp/cnsmo-api/node_modules/cnsmo_web")
+
+    call("echo fase 4 - install cnsmo_web dependencies... >> /var/tmp/slipstream/cnsmo_api_gui.log")
+    call("sudo npm install -g @angular/cli@1.3.2")
+    call("npm install --save-dev @angular/cli@1.3.2")
+    call("npm install")
+
+    call("echo fase 5 - building cnsmo_web...  >> /var/tmp/slipstream/cnsmo_api_gui.log")
+    call("ng build --prod --aot=false")
+    os.chdir("/var/tmp/cnsmo-api")
+    ss_user = call('ss-get cnsmo.user').rstrip('\n')
+    password = ""
+    password = call('ss-random').rstrip('\n')
+    resp = call('ss-set cnsmo.password %s' % password)
+
+    call("echo fase 6 - retrive access credentials and generating config file... >> /var/tmp/slipstream/cnsmo_api_gui.log")
+    call("echo '{\"credentials\": {\"username\": \"%s\",\"password\": \"%s\"}}' > /var/tmp/cnsmo-api/core/config/initConfig.json" % (ss_user,password))
+    call("sudo npm install pm2@latest -g")
+    call("echo fase 7 - starting process... >> /var/tmp/slipstream/cnsmo_api_gui.log")
+    call("pm2 start process.yml")
+    call("echo Done - API and UI installed correctly >> /var/tmp/slipstream/cnsmo_api_gui.log")
+
 def postinstallsdn():
     logger = logging.getLogger(__name__)
     
+    call('pip install requests') 
+    install_gui()
     logger.debug("Set working directory")
     if not os.path.isdir("/opt/odl"):
         os.makedirs("/opt/odl")
