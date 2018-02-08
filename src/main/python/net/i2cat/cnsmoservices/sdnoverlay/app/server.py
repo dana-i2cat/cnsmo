cb3d8fe4186c5b403d277a12d3a0b0a9a2b52368
josep pons <josep.pons@i2cat.net>
2018-02-08 16:53:59 +0100

    issue in monitor method

--- src/main/python/net/i2cat/cnsmoservices/sdnoverlay/app/server.py
+++ src/main/python/net/i2cat/cnsmoservices/sdnoverlay/app/server.py
@@ -176,8 +176,19 @@ def get_all_flow_info(ssinstanceid):
         if openflowID!="":
             url = str("http://127.0.0.1:8080/restconf/operational/opendaylight-inventory:nodes/node/"+openflowID+"/table/0")
             r = requests.get(url , auth=HTTPBasicAuth('admin', 'admin'))
-            
-            return r,200
+            j= r.json()
+            print "hola1"
+            for flow in j['flow-node-inventory:table']:
+
+                print "holain"
+
+                if 'flow' in flow:
+                    print "holaindflow"
+                    for flowdetails in flow['flow']:
+                        if 'id' in flowdetails:
+                            print "id is"
+                            print str(flowdetails['id'])
+                            return str(flowdetails['id']),200
         else:
             return "Node doesn't exist\n", 404
     else:
